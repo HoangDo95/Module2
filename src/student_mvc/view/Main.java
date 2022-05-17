a151481acfbd7243f396132c3fd09b0e7cf02085
dotronghoang95 <dotronghoang95@gmail.com>
2022-05-17 09:31:41 +0700

    MVC

---
+++ src/student_mvc/view/Main.java
@@ -0,0 +1,10 @@
+package student_mvc.view;
+
+import student_mvc.controller.StudentController;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentController.menu();
+
+    }
+}
